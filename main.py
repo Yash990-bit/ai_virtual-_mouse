9efe695d9ff084c7d50cb12b8efe404dc62f2b70
Yash Raghubanshi <yashraghubanshi@Yashs-MacBook-Pro-3.local>
2026-02-19 00:53:11 +0530

    ad

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def main():
     canvas = np.zeros((V_HEIGHT, V_WIDTH, 3), np.uint8)
     draw_mode = False
     prev_draw_x, prev_draw_y = 0, 0
+    prev_draw_x2, prev_draw_y2 = 0, 0
     draw_color = (0, 255, 0) # Default Green
     brush_thickness = 5
     eraser_thickness = 50
@@ -65,104 +66,88 @@ def main():
 
 
         
-        if len(hands_data) == 1:
+        # 1. Whiteboard UI Rendering (Always show in draw mode)
+        if draw_mode:
+            cv2.rectangle(img, (0, 0), (100, 70), (255, 0, 0), cv2.FILLED) # Blue
+            cv2.rectangle(img, (100, 0), (200, 70), (0, 255, 0), cv2.FILLED) # Green
+            cv2.rectangle(img, (200, 0), (300, 70), (0, 0, 255), cv2.FILLED) # Red
+            cv2.rectangle(img, (300, 0), (450, 70), (200, 200, 200), cv2.FILLED) # Eraser
+            cv2.putText(img, "ERASER", (320, 45), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 3)
+            cv2.rectangle(img, (450, 0), (640, 70), (50, 50, 50), cv2.FILLED) # Clear
+            cv2.putText(img, "CLEAR", (480, 45), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
 
-            data = hands_data[0]
+            # Highlight Selected Color
+            if draw_color == (255, 0, 0): cv2.rectangle(img, (0,0), (100, 70), (255,255,100), 4)
+            elif draw_color == (0, 255, 0): cv2.rectangle(img, (100,0), (200, 70), (255,255,100), 4)
+            elif draw_color == (0, 0, 255): cv2.rectangle(img, (200,0), (300, 70), (255,255,100), 4)
+            elif draw_color == (0, 0, 0): cv2.rectangle(img, (300,0), (450, 70), (0,255,0), 4)
+
+        # 2. Process Individual Hand Gestures (Looping over all hands)
+        for i, data in enumerate(hands_data):
             lm = data['lm']
             fingers = data['fingers']
-            x1, y1 = lm[8][1], lm[8][2] # Index
-            x2, y2 = lm[12][1], lm[12][2] # Middle
-            
+            x1, y1 = lm[8][1], lm[8][2]
+            x2, y2 = lm[12][1], lm[12][2]
 
-            initial_dist = 0
+            # --- DRAW MODE LOGIC ---
+            if draw_mode:
+                if fingers[1] == 1:
+                    # Header Interaction (Only first hand can change settings to avoid conflict)
+                    if i == 0 and y1 < 70:
+                        if 0 < x1 < 100: draw_color = (255, 0, 0)
+                        elif 100 < x1 < 200: draw_color = (0, 255, 0)
+                        elif 200 < x1 < 300: draw_color = (0, 0, 255)
+                        elif 300 < x1 < 450: draw_color = (0, 0, 0)
+                        elif 450 < x1 < 640: canvas = np.zeros((V_HEIGHT, V_WIDTH, 3), np.uint8)
+                    
+                    # Drawing (Both hands can draw smoothly!)
+                    elif y1 > 70:
+                        thickness = brush_thickness if draw_color != (0, 0, 0) else eraser_thickness
+                        if i == 0:
+                            if prev_draw_x == 0: prev_draw_x, prev_draw_y = x1, y1
+                            cv2.line(canvas, (prev_draw_x, prev_draw_y), (x1, y1), draw_color, thickness)
+                            prev_draw_x, prev_draw_y = x1, y1
+                        else:
+                            if prev_draw_x2 == 0: prev_draw_x2, prev_draw_y2 = x1, y1
+                            cv2.line(canvas, (prev_draw_x2, prev_draw_y2), (x1, y1), draw_color, thickness)
+                            prev_draw_x2, prev_draw_y2 = x1, y1
+                        
+                        cv2.circle(img, (x1, y1), 8, draw_color, cv2.FILLED)
+                else:
+                    if i == 0: prev_draw_x, prev_draw_y = 0, 0
+                    else: prev_draw_x2, prev_draw_y2 = 0, 0
 
-            if fingers == [0, 0, 0, 0, 0]:
-                if swipe_cooldown == 0:
-                    sys_ctrl.media_control('play_pause')
-                    swipe_cooldown = 40
-                cv2.putText(img, "PLAY/PAUSE", (lm[9][1], lm[9][2]), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
+            # --- NAVIGATION & SYSTEM MODE ---
+            else:
+                if fingers == [0, 0, 0, 0, 0]: # Reset/Pause
+                    if swipe_cooldown == 0: sys_ctrl.media_control('play_pause'); swipe_cooldown = 40
+                
+                elif fingers == [1, 0, 0, 0, 0]: # App Switcher
+                    if swipe_cooldown == 0: sys_ctrl.app_switcher(); swipe_cooldown = 20
 
+                elif fingers == [1, 1, 0, 0, 0]: # Volume Control
+                    if volume_cooldown == 0:
+                        if y1 < 200: sys_ctrl.volume_step('up')
+                        elif y1 > 300: sys_ctrl.volume_step('down')
+                        volume_cooldown = 5
 
-            elif fingers == [1, 0, 0, 0, 0]:
-                if swipe_cooldown == 0:
-                    sys_ctrl.app_switcher()
-                    swipe_cooldown = 20
-
-            elif fingers == [1, 1, 0, 0, 0]:
-                if volume_cooldown == 0:
-                    if y1 < 200: # Hand is high
-                       sys_ctrl.volume_step('up')
-                    elif y1 > 300: # Hand is low
-                       sys_ctrl.volume_step('down')
-                    volume_cooldown = 5
-                    cv2.putText(img, "VOLUME", (x1, y1), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 3)
-
-
-            elif fingers == [0, 0, 0, 0, 1]:
-                if swipe_cooldown == 0:
+                # Pinky UP to toggle draw mode
+                if fingers == [0, 0, 0, 0, 1] and swipe_cooldown == 0:
                     draw_mode = not draw_mode
                     swipe_cooldown = 30
 
-      
-            if draw_mode:
-                # 1. Header Rendering (Colors & Tools)
-                cv2.rectangle(img, (0, 0), (100, 70), (255, 0, 0), cv2.FILLED) # Blue
-                cv2.rectangle(img, (100, 0), (200, 70), (0, 255, 0), cv2.FILLED) # Green
-                cv2.rectangle(img, (200, 0), (300, 70), (0, 0, 255), cv2.FILLED) # Red
-                cv2.rectangle(img, (300, 0), (450, 70), (200, 200, 200), cv2.FILLED) # Eraser
-                cv2.putText(img, "ERASER", (320, 45), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 3)
-                cv2.rectangle(img, (450, 0), (640, 70), (50, 50, 50), cv2.FILLED) # Clear
-                cv2.putText(img, "CLEAR", (480, 45), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+                # Mouse Control (Only first hand controls cursor)
+                if i == 0:
+                    if fingers[1] == 1 and fingers[2] == 0:
+                        mouse.move_cursor(x1, y1, V_WIDTH, V_HEIGHT, margin=FRAME_MARGIN)
+                    elif fingers[1] == 1 and fingers[2] == 1 and np.hypot(x2-x1, y2-y1) < 45:
+                        mouse.click()
+                    elif fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1:
+                        mouse.right_click()
 
-                # Visual Indicator for Selected Color
-                if draw_color == (255, 0, 0): cv2.rectangle(img, (0,0), (100, 70), (255,255,255), 5)
-                elif draw_color == (0, 255, 0): cv2.rectangle(img, (100,0), (200, 70), (255,255,255), 5)
-                elif draw_color == (0, 0, 255): cv2.rectangle(img, (200,0), (300, 70), (255,255,255), 5)
-                elif draw_color == (0, 0, 0): cv2.rectangle(img, (300,0), (450, 70), (0,255,0), 5)
-
-                if fingers[1] == 1:
-                    # Header Interaction Logic
-                    if y1 < 70:
-                        if 0 < x1 < 100:
-                            draw_color = (255, 0, 0)
-                        elif 100 < x1 < 200:
-                            draw_color = (0, 255, 0)
-                        elif 200 < x1 < 300:
-                            draw_color = (0, 0, 255)
-                        elif 300 < x1 < 450:
-                            draw_color = (0, 0, 0) # Eraser Mode
-                        elif 450 < x1 < 640:
-                            canvas = np.zeros((V_HEIGHT, V_WIDTH, 3), np.uint8) # Clear All
-                    
-                    # Drawing Logic
-                    else:
-                        if prev_draw_x == 0: prev_draw_x, prev_draw_y = x1, y1
-                        thickness = brush_thickness if draw_color != (0, 0, 0) else eraser_thickness
-                        cv2.line(canvas, (prev_draw_x, prev_draw_y), (x1, y1), draw_color, thickness)
-                        prev_draw_x, prev_draw_y = x1, y1
-                        # Visual cursor on main image
-                        cv2.circle(img, (x1, y1), thickness//2, draw_color, cv2.FILLED)
-                else: 
-                    prev_draw_x, prev_draw_y = 0, 0
-            else:
-                # 1. Cursor Movement
-                if fingers[1] == 1 and fingers[2] == 0:
-                    mouse.move_cursor(x1, y1, V_WIDTH, V_HEIGHT, margin=FRAME_MARGIN)
-                
-                # 2. Left Click (Pinch Index + Middle)
-                elif fingers[1] == 1 and fingers[2] == 1 and np.hypot(x2-x1, y2-y1) < 45:
-                    mouse.click()
-                    cv2.putText(img, "LEFT CLICK", (x1, y1-20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
-                
-                # 3. Right Click (3 Fingers UP)
-                elif fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1:
-                    mouse.right_click()
-                    cv2.putText(img, "RIGHT CLICK", (x1, y1-20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 3)
-
-        elif len(hands_data) == 2:
-            # --- TWO HAND GESTURES: ZOOM & ROTATE ---
+        # 3. Two-Hand Special Gestures (Zoom & Rotate)
+        if len(hands_data) == 2:
             h1, h2 = hands_data[0]['lm'], hands_data[1]['lm']
-            # Using center of palms (landmark 9)
             cx1, cy1 = h1[9][1], h1[9][2]
             cx2, cy2 = h2[9][1], h2[9][2]
             
@@ -170,10 +155,8 @@ def main():
             curr_angle = math.degrees(math.atan2(cy2 - cy1, cx2 - cx1))
             
             if initial_dist == 0:
-                initial_dist = curr_dist
-                initial_angle = curr_angle
+                initial_dist, initial_angle = curr_dist, curr_angle
             else:
-                # Zoom Logic
                 dist_diff = curr_dist - initial_dist
                 if abs(dist_diff) > 50 and zoom_cooldown == 0:
                     if dist_diff > 0: sys_ctrl.zoom_control('in')
@@ -181,7 +164,6 @@ def main():
                     zoom_cooldown = 10
                     initial_dist = curr_dist
                 
-                # Rotate Logic
                 angle_diff = curr_angle - initial_angle
                 if abs(angle_diff) > 20 and zoom_cooldown == 0:
                     if angle_diff > 0: sys_ctrl.rotate_control('right')
@@ -190,7 +172,9 @@ def main():
                     initial_angle = curr_angle
 
             cv2.line(img, (cx1, cy1), (cx2, cy2), (255, 0, 255), 3)
-            cv2.putText(img, "MULTI-HAND MODE", (200, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 3)
+            cv2.putText(img, "MULTI-HAND ACTIVE", (200, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 3)
+        else:
+            initial_dist = 0
 
         # Merge & Render
         img_gray = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
